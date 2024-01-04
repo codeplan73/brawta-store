f220322917002a54f7c5e6f9c422cc22c32dad98
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2024-01-04 13:03:53 +0100

    added category schema and product schema

--- app/productSchema.ts
+++ app/productSchema.ts
@@ -1,7 +1,17 @@
 import { z } from 'zod'
 
 export const productSchema = z.object({
-  slug: z.string().min(1, 'slug is required').max(255),
-  title: z.string().min(1, 'title is required').max(255),
-  description: z.string().min(1, 'description is required').max(65535),
+  title: z.string().min(3, 'title is required').max(100),
+  description: z.string().min(10, 'description is required').max(65535),
+  price: z.number().min(1, 'price is required'),
+  stock: z.number().int().min(0, 'stock is required'),
+  imageUrl: z.string().url(),
+  category: z.string().min(3).max(50),
 })
+
+export const categorySchema = z.object({
+  title: z.string().min(3, 'title is required').max(100),
+  name: z.string().min(10, 'name is required').max(100),
+})
+
+export type Product = z.infer<typeof productSchema>
